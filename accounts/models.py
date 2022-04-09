0f477d3785a820a3e7824e577aa2cc5b2223cb5a
伊藤裕太 <p3mf5cxkh2@yahoo.co.jp>
2022-04-09 18:06:04 +0900

    CustomUserにuuid追加

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
 
     class Meta:
         verbose_name_plural = 'CustomUser'
